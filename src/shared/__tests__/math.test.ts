d78b824be5f151b0d1fd84681dce3502af48607a
supaiku2452 <supaiku2452@gmail.com>
2020-09-14 02:01:40 +0900

    add spay test.

--- src/shared/__tests__/math.test.ts
+++ src/shared/__tests__/math.test.ts
@@ -1,7 +1,20 @@
-import { sum } from "../math";
+import * as math from "../math";
 
 describe("test", () => {
   test("sum", () => {
-    expect(sum(1, 2)).toEqual(3);
+    expect(math.sum(1, 2)).toEqual(3);
+  });
+  test("spyOn sum", () => {
+    jest.spyOn(math, "sum");
+    expect(math.sum(1, 2)).toEqual(3);
+  });
+  test("spyOn2 sum", () => {
+    const spied = jest.spyOn(math, "sum").mockReturnValue(1);
+    expect(math.sum(1, 2)).toEqual(1);
+    // restoreを忘れると次のテストで失敗する
+    spied.mockRestore();
+  });
+  test("math.sum still mock function", () => {
+    expect(math.sum(1, 2)).toEqual(3);
   });
 });
